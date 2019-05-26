9447a14da6beaaa07c044824cc232c25373560af
charles <twcharley@gmail.com>
2019-05-26 20:53:03 +0800

    P166

---
+++ src/DynamicArray.java
@@ -0,0 +1,18 @@
+
+/*Page 166*/
+public class DynamicArray {
+    public static void main(String[] args){
+        other.main(new String[]{"a","b","c"});
+
+    }
+
+}
+
+
+class other{
+    public static void main(String[] args) {
+        for(String s:args){
+            System.out.println(s);
+        }
+    }
+}
