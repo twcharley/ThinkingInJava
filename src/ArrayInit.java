870e1a66fc85fd843f6c8bfcc9c1b03be4e0211d
charles <twcharley@gmail.com>
2019-05-26 20:43:04 +0800

    P165

---
+++ src/ArrayInit.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class ArrayInit {//page 165
+    public static void main(String[] args) {
+        Integer[] a={
+                new Integer(1),
+                new Integer(2),
+                3,
+        };
+        Integer[] b={
+                new Integer(1),
+                new Integer(2),
+                3
+        };
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+    }
+}
