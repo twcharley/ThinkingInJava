1a2bab98163691644607e6c0b560b13ea2e4f60c
charles <twcharley@gmail.com>
2019-05-26 20:42:32 +0800

    167

---
+++ src/VarArgs.java
@@ -0,0 +1,19 @@
+/*page 167*/
+
+public class VarArgs {
+    static void printArray(Object[] args){
+        for (Object obj:args)
+            System.out.println(obj+" ");
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        printArray(new Object[]{
+                new Integer(47),new Float(3.14),new Double(11.11),
+        });
+        printArray(new Object[]{"One","Two","Thtee",});
+        printArray(new Object[]{new A(),new A(),new A()});
+    }
+}
+class A{}
